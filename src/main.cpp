22a0aab18a257adddd514aeda8a02190a6ab60aa
Meysam Parvizi <meysam.parvizi.ir@gmail.com>
2022-11-11 23:31:25 +0330

    Test FreeRTOS task Suspend, Resume, and Delete

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,17 @@
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
 
+// LED pins
 static const gpio_num_t led_pin = GPIO_NUM_5; // LED connected to GPIO5 (On-board LED)
-static const gpio_num_t led_red = GPIO_NUM_18;
-static const gpio_num_t led_green = GPIO_NUM_19;
-static const gpio_num_t led_blue = GPIO_NUM_23;
+static const gpio_num_t led_red = GPIO_NUM_19;
+static const gpio_num_t led_green = GPIO_NUM_23;
+static const gpio_num_t led_blue = GPIO_NUM_18;
+
+// Task handles
+static TaskHandle_t TaskHandle_LedOnBoard = NULL;
+static TaskHandle_t TaskHandle_LedRed = NULL;
+static TaskHandle_t TaskHandle_LedGreen = NULL;
+static TaskHandle_t TaskHandle_LedBlue = NULL;
 
 void my_config_led_pin(gpio_num_t gpio_num)
 {
@@ -17,24 +24,28 @@ void my_config_led_pin(gpio_num_t gpio_num)
   io_conf.pull_up_en = GPIO_PULLUP_DISABLE;
   gpio_config(&io_conf);
 }
- 
-void my_led_blink(gpio_num_t gpio_num)
+
+void my_led_blink_fast(gpio_num_t gpio_num)
 {
   gpio_set_level(gpio_num, 0);
   vTaskDelay(100 / portTICK_RATE_MS);
   gpio_set_level(gpio_num, 1);
-  vTaskDelay(200 / portTICK_RATE_MS);
-  gpio_set_level(gpio_num, 0);
   vTaskDelay(100 / portTICK_RATE_MS);
+}
+
+void my_led_blink_slow(gpio_num_t gpio_num)
+{
+  gpio_set_level(gpio_num, 0);
+  vTaskDelay(1000 / portTICK_RATE_MS);
   gpio_set_level(gpio_num, 1);
-  vTaskDelay(600 / portTICK_RATE_MS);
+  vTaskDelay(1000 / portTICK_RATE_MS);
 }
 
 void vTaskLedOnBoard(void *pvParameters)
 {
   while (1)
   {
-    my_led_blink(led_pin);
+    my_led_blink_fast(led_pin);
   }
 }
 
@@ -42,7 +53,7 @@ void vTaskLedRed(void *pvParameters)
 {
   while (1)
   {
-    my_led_blink(led_red);
+    my_led_blink_slow(led_red);
   }
 }
 
@@ -50,7 +61,7 @@ void vTaskLedGreen(void *pvParameters)
 {
   while (1)
   {
-    my_led_blink(led_green);
+    my_led_blink_slow(led_green);
   }
 }
 
@@ -58,7 +69,7 @@ void vTaskLedBlue(void *pvParameters)
 {
   while (1)
   {
-    my_led_blink(led_blue);
+    my_led_blink_slow(led_blue);
   }
 }
 
@@ -72,40 +83,40 @@ extern "C" void app_main(void)
 
   // Create tasks
   xTaskCreate(
-      vTaskLedOnBoard,   // Task handler function
-      "vTaskLedOnBoard", // Task name (used for debugging)
+      vTaskLedOnBoard,       // Task handler function
+      "vTaskLedOnBoard",     // Task name (used for debugging)
+      1024,                  // Stack depth of this task (in words)
+      NULL,                  // Parameters passed to task handler function
+      2,                     // Task priority
+      &TaskHandle_LedOnBoard // TaskHandle_t reference variable of this task (used for changing priority or delete task in program)
+  );
+
+  xTaskCreate(
+      vTaskLedRed,       // Task handler function
+      "vTaskLedRed",     // Task name (used for debugging)
       1024,              // Stack depth of this task (in words)
       NULL,              // Parameters passed to task handler function
       1,                 // Task priority
-      NULL               // TaskHandle_t reference variable of this task (used for changing priority or delete task in program)
+      &TaskHandle_LedRed // TaskHandle_t reference variable of this task (used for changing priority or delete task in program)
   );
 
-  xTaskCreate(
-      vTaskLedRed,   // Task handler function
-      "vTaskLedRed", // Task name (used for debugging)
-      1024,          // Stack depth of this task (in words)
-      NULL,          // Parameters passed to task handler function
-      1,             // Task priority
-      NULL           // TaskHandle_t reference variable of this task (used for changing priority or delete task in program)
-  );
+  // xTaskCreate(
+  //     vTaskLedGreen,       // Task handler function
+  //     "vTaskLedGreen",     // Task name (used for debugging)
+  //     1024,                // Stack depth of this task (in words)
+  //     NULL,                // Parameters passed to task handler function
+  //     1,                   // Task priority
+  //     &TaskHandle_LedGreen // TaskHandle_t reference variable of this task (used for changing priority or delete task in program)
+  // );
 
-  xTaskCreate(
-      vTaskLedGreen,   // Task handler function
-      "vTaskLedGreen", // Task name (used for debugging)
-      1024,            // Stack depth of this task (in words)
-      NULL,            // Parameters passed to task handler function
-      1,               // Task priority
-      NULL             // TaskHandle_t reference variable of this task (used for changing priority or delete task in program)
-  );
-
-  xTaskCreate(
-      vTaskLedBlue,   // Task handler function
-      "vTaskLedBlue", // Task name (used for debugging)
-      1024,           // Stack depth of this task (in words)
-      NULL,           // Parameters passed to task handler function
-      1,              // Task priority
-      NULL            // TaskHandle_t reference variable of this task (used for changing priority or delete task in program)
-  );
+  // xTaskCreate(
+  //     vTaskLedBlue,       // Task handler function
+  //     "vTaskLedBlue",     // Task name (used for debugging)
+  //     1024,               // Stack depth of this task (in words)
+  //     NULL,               // Parameters passed to task handler function
+  //     1,                  // Task priority
+  //     &TaskHandle_LedBlue // TaskHandle_t reference variable of this task (used for changing priority or delete task in program)
+  // );
 
   // Start the scheduler so the tasks start executing
   /* NOTE: In ESP-IDF the scheduler is started automatically during
@@ -113,4 +124,23 @@ extern "C" void app_main(void)
    * ESP-IDF applications.
    */
   // vTaskStartScheduler();
+
+  while (1)
+  {
+    // Suspend the higher priority task for some intervals
+    for (int i = 0; i < 3; i++)
+    {
+      vTaskSuspend(TaskHandle_LedOnBoard);
+      vTaskDelay(2000 / portTICK_PERIOD_MS);
+      vTaskResume(TaskHandle_LedOnBoard);
+      vTaskDelay(2000 / portTICK_PERIOD_MS);
+    }
+
+    // Delete the lower priority task
+    if (TaskHandle_LedRed != NULL)
+    {
+      vTaskDelete(TaskHandle_LedRed);
+      TaskHandle_LedRed = NULL;
+    }
+  }
 }
